e048a9772e59d16664ac03e07085b93b3a9bcddf
Kerevizxd <87535984+Kerevizxd@users.noreply.github.com>
2021-09-11 21:40:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// the nav toggle
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
